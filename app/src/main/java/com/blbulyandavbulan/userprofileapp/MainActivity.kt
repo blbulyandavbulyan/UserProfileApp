9d8834064cdc7e8860ba70ed014b49ab7a1e3947
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-03-29 15:21:03 +0400

    add showing random score in MainActivity

--- app/src/main/java/com/blbulyandavbulan/userprofileapp/MainActivity.kt
+++ app/src/main/java/com/blbulyandavbulan/userprofileapp/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.blbulyandavbulan.userprofileapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.blbulyandavbulan.userprofileapp.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val userScore = Random.nextInt(101)
+        binding.scoreIndicator.setProgress(userScore, true)
+        binding.scoreValueTooltip.text = getString(R.string.score_value_template, userScore)
     }
 }
