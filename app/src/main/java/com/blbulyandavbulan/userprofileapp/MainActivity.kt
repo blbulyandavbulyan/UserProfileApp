8158a36ba1975e4ab6c9e5972daa93ae239231a0
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-03-29 15:51:58 +0400

    now notifications checkboxes are located in LinearLayout added processing for notificationSwitch in MainActivity

--- app/src/main/java/com/blbulyandavbulan/userprofileapp/MainActivity.kt
+++ app/src/main/java/com/blbulyandavbulan/userprofileapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.blbulyandavbulan.userprofileapp
 
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.allViews
 import com.blbulyandavbulan.userprofileapp.databinding.ActivityMainBinding
 import kotlin.random.Random
 
@@ -13,5 +16,15 @@ class MainActivity : AppCompatActivity() {
         val userScore = Random.nextInt(101)
         binding.scoreIndicator.setProgress(userScore, true)
         binding.scoreValueTooltip.text = getString(R.string.score_value_template, userScore)
+        binding.notificationSwitch.setOnCheckedChangeListener { _: CompoundButton, isChecked: Boolean ->
+            binding.notificationsCheckboxes.allViews.forEach {
+                if (isChecked) it.isEnabled = true
+                else if (it is CheckBox) {
+                    it.isEnabled = false
+                    it.isChecked = false
+                }
+            }
+        }
+
     }
 }
