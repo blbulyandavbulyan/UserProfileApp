8aa668f825a00918bfac40b785c49324795d8b38
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-03-29 17:51:31 +0400

    added changing enabled state for button save in MainActivity

--- app/src/main/java/com/blbulyandavbulan/userprofileapp/MainActivity.kt
+++ app/src/main/java/com/blbulyandavbulan/userprofileapp/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.blbulyandavbulan.userprofileapp
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.allViews
+import androidx.core.widget.addTextChangedListener
 import com.blbulyandavbulan.userprofileapp.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private var authorizationNotificationsEnabled = false
+    private var notificationsSwitchEnabled = false
+    private var newProductsAndOffersNotificationsEnabled = false
+    private var nameFieldValid = false
+    private var phoneFieldValid = false
+    private var validGenderSelected = false;
+    private var saveButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         val userScore = Random.nextInt(101)
         binding.scoreIndicator.setProgress(userScore, true)
         binding.scoreValueTooltip.text = getString(R.string.score_value_template, userScore)
+        saveButton = binding.saveButton
         binding.notificationSwitch.setOnCheckedChangeListener { _: CompoundButton, isChecked: Boolean ->
             binding.notificationsCheckboxes.allViews.forEach {
                 if (isChecked) it.isEnabled = true
@@ -24,7 +34,37 @@ class MainActivity : AppCompatActivity() {
                     it.isChecked = false
                 }
             }
+            notificationsSwitchEnabled = isChecked
+            recalculateButtonEnabledState()
         }
+        binding.newProductsAndOffersNotifications.setOnCheckedChangeListener { _, isChecked ->
+            newProductsAndOffersNotificationsEnabled = isChecked
+            recalculateButtonEnabledState()
+        }
+        binding.authorizationNotifications.setOnCheckedChangeListener { _, isChecked ->
+            authorizationNotificationsEnabled = isChecked
+            recalculateButtonEnabledState()
+        }
+        val profileName = binding.profileName
+        profileName.editText?.addTextChangedListener {
+            val length = it?.length ?: 0
+            nameFieldValid = length > 0 && length <= profileName.counterMaxLength
+            recalculateButtonEnabledState()
+        }
+        binding.userPhone.editText?.addTextChangedListener {
+            phoneFieldValid = it?.isNotEmpty() ?: false
+            recalculateButtonEnabledState()
+        }
+        binding.genders.setOnCheckedChangeListener { _, checkedId ->
+            validGenderSelected = checkedId != -1
+            recalculateButtonEnabledState()
+        }
+    }
 
+    private fun recalculateButtonEnabledState() {
+        val notificationStateValid =
+            !notificationsSwitchEnabled || authorizationNotificationsEnabled || newProductsAndOffersNotificationsEnabled
+        saveButton?.isEnabled =
+            validGenderSelected && nameFieldValid && phoneFieldValid && notificationStateValid
     }
 }
